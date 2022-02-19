62a7ff404ef8d37ddf81185b7028ea4f1488b472
624167284@qq.com <624167284@qq.com>
2022-02-19 18:05:30 +0800

    clipboard

---
+++ packages/vue-clipboard-x/README.md
@@ -0,0 +1,3 @@
+# @ruiku/vue-clipboard-x
+真正适配vue3的剪切板
+基本用法参见 [vue-clipboard2](https://github.com/Inndy/vue-clipboard2#readme)
---
+++ packages/vue-clipboard-x/stories/vue-clipboard.stories.js
@@ -0,0 +1,37 @@
+import VueClipboard from '..'
+
+import {
+  createApp
+} from 'vue'
+
+// const app = createApp()
+// app.use(VueClipboard)
+// console.log(VueClipboard)
+// console.log(app)
+
+export default {
+  title:"VueClipboard",
+  component:document.body
+}
+
+export const ClipBoard = (...args)=>{
+  return {
+    template:`<div
+    v-clipboard:copy="text"
+    v-clipboard:success="onCopySuccess"
+    v-clipboard:error="onCopyError">拷贝</div>`,
+    data(){
+      return {
+        text:"拷贝的文字"
+      }
+    },
+    methods:{
+      onCopySuccess(){
+        alert("拷贝成功")
+      },
+      onCopyError(){
+        alert("拷贝失败")
+      }
+    }
+  }
+}
